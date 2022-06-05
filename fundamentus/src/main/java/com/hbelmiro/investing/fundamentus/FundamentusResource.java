155e9738d1c159c871a5aa5b740eb61dcdb71f0c
Helber Belmiro <helber.belmiro@gmail.com>
2022-06-05 18:20:20 -0300

    Indicators formatted to paste on Google Docs
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- fundamentus/src/main/java/com/hbelmiro/investing/fundamentus/FundamentusResource.java
+++ fundamentus/src/main/java/com/hbelmiro/investing/fundamentus/FundamentusResource.java
@@ -5,10 +5,40 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/hello")
 public class FundamentusResource {
+
+    private static final NumberFormat currencyFormatter = NumberFormat.getInstance(new Locale("pt", "BR"));
+
+    private static final List<String> tickers = List.of(
+            "GRND3",
+            "ITUB3",
+            "EGIE3",
+            "ENBR3",
+            "TAEE11",
+            "FLRY3",
+            "WEGE3",
+            "LREN3",
+            "MDIA3",
+            "BIDI11",
+            "BBDC3",
+            "BBAS3",
+            "BRSR6",
+            "EZTC3",
+            "MGLU3",
+            "PSSA3",
+            "ODPV3",
+            "XPBR31",
+            "ARZZ3"
+    );
 
     private final FundamentusClient fundamentusClient;
 
@@ -19,7 +49,35 @@ public class FundamentusResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws IOException {
-        return fundamentusClient.read("ITUB3").toString();
+    public String hello() {
+        Map<String, Optional<Indicators>> map = new LinkedHashMap<>(tickers.size());
+
+        tickers.forEach(ticker -> {
+            Optional<Indicators> indicators;
+            try {
+                indicators = Optional.of(fundamentusClient.read(ticker));
+            } catch (Exception e) {
+                indicators = Optional.empty();
+            }
+            map.put(ticker, indicators);
+        });
+
+        String lpa = map.values().stream()
+                             .map(indicators -> indicators.map(Indicators::lpa).map(currencyFormatter::format).orElse("Error"))
+                             .collect(Collectors.joining(System.lineSeparator()));
+
+        String dy = map.values().stream()
+                            .map(indicators -> indicators.map(Indicators::dividendYield).map(currencyFormatter::format).orElse("Error"))
+                            .collect(Collectors.joining(System.lineSeparator()));
+
+        return """
+                LPA
+                ===
+                """ + lpa + """
+                  
+                                
+                DY
+                ==
+                """ + dy;
     }
 }
