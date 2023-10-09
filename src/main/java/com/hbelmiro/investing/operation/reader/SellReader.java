a08a0ed6d8267887037c559fc9e7cd3534ea5d92
Helber Belmiro <helber.belmiro@gmail.com>
2023-10-09 19:59:26 -0300

    SellReader and BuyReader now extends OperationReader (#35)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/main/java/com/hbelmiro/investing/operation/reader/SellReader.java
+++ src/main/java/com/hbelmiro/investing/operation/reader/SellReader.java
@@ -1,54 +1,22 @@
 package com.hbelmiro.investing.operation.reader;
 
-import com.hbelmiro.investing.Operation;
 import com.hbelmiro.investing.OperationType;
-import com.hbelmiro.investing.Stock;
 import com.hbelmiro.investing.googlesheets.GoogleSheetsClient;
 import jakarta.enterprise.context.ApplicationScoped;
-
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.math.BigDecimal;
-import java.security.GeneralSecurityException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
-final class SellReader {
+final class SellReader extends OperationReader {
 
-    private static final String PAGE = "Vendas Ações BR";
+    /**
+     * @deprecated must be used only by CDI
+     */
+    @Deprecated(forRemoval = true)
+    SellReader() {
+    }
 
-    public static final String RANGE = "A3:F";
-
-    private final GoogleSheetsClient googleSheetsClient;
-
+    @Inject
     SellReader(GoogleSheetsClient googleSheetsClient) {
-        this.googleSheetsClient = googleSheetsClient;
-    }
-
-    List<Operation> read() throws GeneralSecurityException {
-        try {
-            return googleSheetsClient.read(PAGE, RANGE).stream()
-                    .map(SellReader::toOperation)
-                    .toList();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private static Operation toOperation(List<Object> row) {
-        return Operation.builder()
-                .type(OperationType.SELL)
-                .date(LocalDate.parse(row.get(0).toString(), DateTimeFormatter.ofPattern("dd/MM/yyyy")))
-                .stock(new Stock(row.get(1).toString()))
-                .amount(new BigDecimal(row.get(2).toString().replace(".", "").replace(",", ".")))
-                .price(toMoney(row.get(3).toString()))
-                .tax(toMoney(row.get(4).toString()).add(toMoney(row.get(5).toString())))
-                .build();
-    }
-
-    private static BigDecimal toMoney(String value) {
-        return new BigDecimal(value.replace("R$ ", "").replace(".", "").replace(",", "."));
+        super("Vendas Ações BR", googleSheetsClient, OperationType.SELL);
     }
 }
