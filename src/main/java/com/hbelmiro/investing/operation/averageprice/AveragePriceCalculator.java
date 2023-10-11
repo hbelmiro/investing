5b7cbee7f482ffeccfd5e7ef0d7a0fb41299f149
Helber Belmiro <helber.belmiro@gmail.com>
2023-10-10 22:38:55 -0300

    Average price calculation (#38)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/main/java/com/hbelmiro/investing/operation/averageprice/AveragePriceCalculator.java
+++ src/main/java/com/hbelmiro/investing/operation/averageprice/AveragePriceCalculator.java
@@ -1,29 +1,74 @@
 package com.hbelmiro.investing.operation.averageprice;
 
 import com.hbelmiro.investing.Operation;
+import com.hbelmiro.investing.OperationType;
 import com.hbelmiro.investing.Stock;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.javamoney.moneta.Money;
 
+import javax.money.Monetary;
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 
+@ApplicationScoped
 final class AveragePriceCalculator {
+
+    public static final String DIFFERENT_STOCK_ERROR_MSG = "Operations must be from the same stock";
+
+    public static final String DIFFERENT_PRICE_CURRENCY_ERROR_MSG = "Prices must be all in the same currency";
+
+    public static final String DIFFERENT_TAX_CURRENCY_ERROR_MSG = "Taxes must be all in the same currency";
 
     private AveragePriceCalculator() {
     }
 
-    BigDecimal calculate(List<Operation> operations) {
+    Money calculate(List<Operation> operations) {
         validate(operations);
 
-        throw new UnsupportedOperationException("Not implemented yet");
+        Money totalPrice = Money.zero(operations.get(0).getPrice().getCurrency());
+        BigDecimal totalAmount = BigDecimal.ZERO;
+
+        List<Operation> sortedOperations = operations.stream().sorted(Comparator.comparing(Operation::getDate)).toList();
+
+        for (Operation op : sortedOperations) {
+            if (op.getType() == OperationType.BUY) {
+                Money opValue = op.getPrice().add(op.getTax()).multiply(op.getAmount());
+                totalPrice = totalPrice.add(opValue);
+                totalAmount = totalAmount.add(op.getAmount());
+            } else {
+                Money currentAverage = totalPrice.divide(totalAmount);
+                totalPrice = totalPrice.subtract(currentAverage.multiply(op.getAmount()));
+                totalAmount = totalAmount.subtract(op.getAmount());
+            }
+        }
+
+        return totalPrice.divide(totalAmount).with(Monetary.getDefaultRounding());
     }
 
     private static void validate(List<Operation> operations) {
         if (operations.stream()
-                      .map(Operation::getStock)
-                      .map(Stock::symbol)
-                      .distinct()
-                      .count() > 1) {
-            throw new IllegalArgumentException("Operations must be from the same stock");
+                .map(Operation::getStock)
+                .map(Stock::symbol)
+                .distinct()
+                .count() > 1) {
+            throw new IllegalArgumentException(DIFFERENT_STOCK_ERROR_MSG);
+        }
+
+        if (operations.stream()
+                .map(Operation::getPrice)
+                .map(Money::getCurrency)
+                .distinct()
+                .count() > 1) {
+            throw new IllegalArgumentException(DIFFERENT_PRICE_CURRENCY_ERROR_MSG);
+        }
+
+        if (operations.stream()
+                .map(Operation::getTax)
+                .map(Money::getCurrency)
+                .distinct()
+                .count() > 1) {
+            throw new IllegalArgumentException(DIFFERENT_TAX_CURRENCY_ERROR_MSG);
         }
     }
 }
--- src/test/java/com/hbelmiro/investing/operation/averageprice/AveragePriceCalculatorTest.java
+++ src/test/java/com/hbelmiro/investing/operation/averageprice/AveragePriceCalculatorTest.java
@@ -1,5 +1,157 @@
 package com.hbelmiro.investing.operation.averageprice;
 
+import com.hbelmiro.investing.Operation;
+import com.hbelmiro.investing.OperationType;
+import com.hbelmiro.investing.Stock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Test;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.hbelmiro.investing.operation.averageprice.AveragePriceCalculator.DIFFERENT_PRICE_CURRENCY_ERROR_MSG;
+import static com.hbelmiro.investing.operation.averageprice.AveragePriceCalculator.DIFFERENT_STOCK_ERROR_MSG;
+import static com.hbelmiro.investing.operation.averageprice.AveragePriceCalculator.DIFFERENT_TAX_CURRENCY_ERROR_MSG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@QuarkusTest
 class AveragePriceCalculatorTest {
 
+    @Inject
+    AveragePriceCalculator averagePriceCalculator;
+
+    private static final CurrencyUnit DOLLARS = Monetary.getCurrency("USD");
+
+    private static final CurrencyUnit REAIS = Monetary.getCurrency("BRL");
+
+    @Test
+    void calculate() {
+        Operation op1 = Operation.builder()
+                .date(LocalDate.of(2020, 1, 1))
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(50, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        Operation op2 = Operation.builder()
+                .date(LocalDate.of(2020, 1, 2))
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(100, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.ONE)
+                .build();
+
+        Operation op3 = Operation.builder()
+                .date(LocalDate.of(2020, 1, 3))
+                .type(OperationType.SELL)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(75, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(new BigDecimal(5))
+                .build();
+
+        Operation op4 = Operation.builder()
+                .date(LocalDate.of(2020, 1, 4))
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(75, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(new BigDecimal(100))
+                .build();
+
+        List<Operation> operations = List.of(op1, op2, op3, op4);
+
+        assertThat(averagePriceCalculator.calculate(operations))
+                .isEqualTo(Money.of(new BigDecimal("73.89"), DOLLARS));
+    }
+
+    @Test
+    void operationsForNonDistinctStocksShouldThrowException() {
+        Operation op1 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(12, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        Operation op2 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("BBBB"))
+                .price(Money.of(12, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        List<Operation> operations = List.of(op1, op2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> averagePriceCalculator.calculate(operations))
+                .withMessage(DIFFERENT_STOCK_ERROR_MSG);
+    }
+
+    @Test
+    void operationsForNonDistinctCurrencyPriceShouldThrowException() {
+        Operation op1 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(12, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        Operation op2 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(12, REAIS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        List<Operation> operations = List.of(op1, op2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> averagePriceCalculator.calculate(operations))
+                .withMessage(DIFFERENT_PRICE_CURRENCY_ERROR_MSG);
+    }
+
+    @Test
+    void operationsForNonDistinctCurrencyTaxShouldThrowException() {
+        Operation op1 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(12, DOLLARS))
+                .tax(Money.of(0.05, DOLLARS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        Operation op2 = Operation.builder()
+                .date(LocalDate.now())
+                .type(OperationType.BUY)
+                .stock(new Stock("AAAA"))
+                .price(Money.of(12, DOLLARS))
+                .tax(Money.of(0.05, REAIS))
+                .amount(BigDecimal.TEN)
+                .build();
+
+        List<Operation> operations = List.of(op1, op2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> averagePriceCalculator.calculate(operations))
+                .withMessage(DIFFERENT_TAX_CURRENCY_ERROR_MSG);
+    }
 }
