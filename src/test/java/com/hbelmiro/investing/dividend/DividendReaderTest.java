f4cd7bd3bd6a9d26eb91f3063a1bb7a253c50089
Helber Belmiro <helber.belmiro@gmail.com>
2023-10-17 20:30:46 -0300

    Added DividendReader (#44)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ src/test/java/com/hbelmiro/investing/dividend/DividendReaderTest.java
@@ -0,0 +1,61 @@
+package com.hbelmiro.investing.dividend;
+
+import com.hbelmiro.investing.asset.Asset;
+import com.hbelmiro.investing.currency.CurrencyCode;
+import com.hbelmiro.investing.googlesheets.CsvGoogleSheetsClient;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Test;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.math.BigDecimal;
+import java.security.GeneralSecurityException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+class DividendReaderTest {
+
+    private static final CurrencyUnit BRL = Monetary.getCurrency(CurrencyCode.BRL);
+
+    @Inject
+    CsvGoogleSheetsClient csvGoogleSheetsClient;
+
+    @Inject
+    DividendReader dividendReader;
+
+    @Test
+    void readDividends() throws GeneralSecurityException {
+        csvGoogleSheetsClient.setCsv("/csv/DividendReader/readDividends.csv");
+
+        List<Dividend> dividends = dividendReader.read();
+
+        Dividend d1 = new Dividend(
+                LocalDate.of(2019, 3, 15),
+                DividendType.DIVIDEND,
+                Money.of(new BigDecimal("32.26"), BRL),
+                new Asset("ITUB3", BRL)
+        );
+
+        Dividend d2 = new Dividend(
+                LocalDate.of(2019, 3, 21),
+                DividendType.INTEREST_ON_EQUITY,
+                Money.of(new BigDecimal("43.90"), BRL),
+                new Asset("MDIA3", BRL)
+        );
+
+        Dividend d3 = new Dividend(
+                LocalDate.of(2019, 3, 21),
+                DividendType.FRACTIONS,
+                Money.of(new BigDecimal("18.55"), BRL),
+                new Asset("WEGE3", BRL)
+        );
+
+        assertThat(dividends)
+                .containsExactlyInAnyOrder(d1, d2, d3);
+    }
+}
