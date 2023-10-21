c91fe0c1da4d9511a2df32e148b4e0c2872542d1
Helber Belmiro <helber.belmiro@gmail.com>
2023-10-21 13:12:49 -0300

    Fixed tests (#51)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/test/java/com/hbelmiro/investing/operation/reader/BrStocksBuyReaderTest.java
+++ src/test/java/com/hbelmiro/investing/operation/reader/BrStocksBuyReaderTest.java
@@ -4,12 +4,19 @@ import com.hbelmiro.investing.OperationType;
 import com.hbelmiro.investing.googlesheets.CsvGoogleSheetsClient;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 
 @QuarkusTest
 class BrStocksBuyReaderTest extends OperationReaderTest {
 
     @Inject
-    BrStocksBuyReaderTest(BrStocksBuyReader reader, CsvGoogleSheetsClient googleSheetsClient) {
-        super(reader, googleSheetsClient, OperationType.BUY);
+    BrStocksBuyReader reader;
+
+    @Inject
+    CsvGoogleSheetsClient googleSheetsClient;
+
+    @BeforeEach
+    void setup() {
+        initialize(reader, googleSheetsClient, OperationType.BUY);
     }
 }
--- src/test/java/com/hbelmiro/investing/operation/reader/BrStocksSellReaderTest.java
+++ src/test/java/com/hbelmiro/investing/operation/reader/BrStocksSellReaderTest.java
@@ -4,12 +4,19 @@ import com.hbelmiro.investing.OperationType;
 import com.hbelmiro.investing.googlesheets.CsvGoogleSheetsClient;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 
 @QuarkusTest
 class BrStocksSellReaderTest extends OperationReaderTest {
 
     @Inject
-    BrStocksSellReaderTest(BrStocksSellReader reader, CsvGoogleSheetsClient googleSheetsClient) {
-        super(reader, googleSheetsClient, OperationType.SELL);
+    BrStocksSellReader reader;
+
+    @Inject
+    CsvGoogleSheetsClient googleSheetsClient;
+
+    @BeforeEach
+    void setup() {
+        initialize(reader, googleSheetsClient, OperationType.SELL);
     }
 }
