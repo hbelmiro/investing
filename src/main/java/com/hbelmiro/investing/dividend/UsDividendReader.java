440137963da4ca63649f7e99100a3ee831424e79
Helber Belmiro <helber.belmiro@gmail.com>
2026-05-18 09:03:50 -0300

    refactor: introduce ColumnMapping in DividendReader and adapt readers/tests (#278)

--- src/main/java/com/hbelmiro/investing/dividend/UsDividendReader.java
+++ src/main/java/com/hbelmiro/investing/dividend/UsDividendReader.java
@@ -8,16 +8,18 @@ import jakarta.inject.Inject;
 @ApplicationScoped
 public final class UsDividendReader extends DividendReader {
 
+    private static final ColumnMapping COLUMN_MAPPING = new ColumnMapping("A2:N", 0, 2, 3, 4, 6);
+
     /**
      * @deprecated must be used only by CDI
      */
     @Deprecated(forRemoval = true)
     UsDividendReader() {
-        super(null, null, null);
+        super(null, null, null, null);
     }
 
     @Inject
     public UsDividendReader(GoogleSheetsClient googleSheetsClient) {
-        super(googleSheetsClient, "Proventos US", MoneyUtil.USD);
+        super(googleSheetsClient, "Proventos US", MoneyUtil.USD, COLUMN_MAPPING);
     }
 }
